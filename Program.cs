dabcf6e039540d4d9311b74c289a380659da5ae0
JRSerjeant <Jack.Serjeant@googlemail.com>
2014-08-21 08:57:06 +0100

    First Commit

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: Jack
+ * Date: 21/08/2014
+ * Time: 08:53
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace GitTest
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
